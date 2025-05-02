ce6b4b4fd1f9343ddefc9e7f817a0ac0aa16ad50
Sergio García <sergio4geeks@gmail.com>
2025-05-02 20:17:43 +0000

    finished

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,48 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+	const Home = () => {
+	const [newTask, setNewTask] = useState ('');
+	const [taskList, setTaskList] = useState ([]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	const onChange = (event) => {
+		setNewTask(event.target.value);
+	}
+	const deleteTask = (taskToDelete) => {
+		setTaskList(taskList.filter(task => task !== taskToDelete));
+	}
+	const onKeyDown = (event) => {
+		if (event.keyCode === 13) {
+		event.preventDefault();
+		if (newTask !== '') {
+			setTaskList([...taskList, newTask]);
+			setNewTask('');
+		}
+		}
+	};
+	const onClick = (event) => {
+		event.preventDefault();
+		if (newTask !== '') {setTaskList([...taskList, newTask]);
+			setNewTask('');}
+	}
 
+		return (
+			<div className="list">
+				<h3>Todos</h3>
+					<div className="card">
+							<form onChange={onChange} onSubmit={onClick}>
+								<input placeholder="add your task" className="task" onKeyDown={onKeyDown} value={newTask} />
+							</form>
+						<div className="taskButton">
+						{taskList.length === 0 ? ("") :(
+							taskList.map((task,index) => <p className="taskItem" key={index}> {task} <button  onClick={() => deleteTask(task)}>❌</button> </p>))}
+						</div>
+						<div>
+						<p className="itemLeft">{taskList.length} Item{taskList.length !== 1 ? 's' : ''} left</p>
+						</div>
+					</div>
+			</div>
+		);
+	};
 export default Home;
